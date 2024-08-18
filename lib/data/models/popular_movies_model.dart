5c333c5566f7f063739f857fb3c8c56b2e77c1b3
Rana Ahsan Ali <ranaahsanali@gmail.com>
2024-08-18 04:40:02 +0400

    update models

--- lib/data/models/popular_movies_model.dart
+++ lib/data/models/popular_movies_model.dart
@@ -1,15 +1,18 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:movies_app/data/models/movie_model.dart';
 
 part 'popular_movies_model.g.dart';
 
 @JsonSerializable()
 class PopularMoviesModel {
   int? page;
+  List<MovieModel>? movies;
   int? total_pages;
   int? total_results;
 
   PopularMoviesModel({
     this.page,
+    this.movies,
     this.total_pages,
     this.total_results
   });
