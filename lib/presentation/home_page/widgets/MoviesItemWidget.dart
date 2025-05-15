5ec1b57e620666fd1863830fb27f11273874d641
Rana Ahsan Ali <ranaahsanali@gmail.com>
2025-05-15 15:39:55 +0300

    fix lint

--- lib/presentation/home_page/widgets/MoviesItemWidget.dart
+++ lib/presentation/home_page/widgets/MoviesItemWidget.dart
@@ -10,7 +10,7 @@ class MoviesItemView extends StatelessWidget{
 
   final MovieModel item;
 
-  MoviesItemView(this.item, {super.key});
+  const MoviesItemView(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
